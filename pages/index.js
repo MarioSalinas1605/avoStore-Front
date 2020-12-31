49ba497c22623e4f15a80956541ad3cfcffae6ea
Mario Salinas <mario_1605@live.com.mx>
2020-12-31 07:27:24 -0600

    feat: fetch to api of all avocados and by id

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Navbar from '../components/Navbar/Navbar'
 
 const Home = () => {
+    const [productList, setProductList] = useState([])
+
+    useEffect(() => {
+        window
+            .fetch('/api/avo')
+            .then(response => response.json())
+            .then(({ data, length}) => {
+                setProductList(data)
+            })
+    }, [])
+
     return (
         <div>
             <Navbar />
             <h1>Hola Platzi!</h1>
+            {
+                productList.map(product => (
+                    <div key={product.id}>{product.name}</div>
+                ))
+            }
         </div>
     )
 }
--- pages/product/[id].js
+++ pages/product/[id].js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 
 const ProductItem = () => {
     const { query: { id } } = useRouter()
+    const [product, setProduct] = useState({})
+
+    useEffect(() => {
+        console.log(id);
+        if (id) {
+            window.fetch(`/api/avo/${id}`)
+                .then(response => response.json())
+                .then(data => setProduct(data))
+        }
+        
+    }, [id])
+
     return (
         <div>
             Esta es la página del producto: {id}
+            <p>{product.name}</p>
         </div>
     )
 }
