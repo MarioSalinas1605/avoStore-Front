dce0f188b4c2e59dc27f06899d3532bdba7d14fd
Mario Salinas <mario_1605@live.com.mx>
2021-01-01 12:41:40 -0600

    feat: extend app for layout

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-import Navbar from '../components/Navbar/Navbar'
 
 const Home = () => {
     const [productList, setProductList] = useState([])
@@ -15,7 +14,6 @@ const Home = () => {
 
     return (
         <div>
-            <Navbar />
             <h1>Hola Platzi!</h1>
             {
                 productList.map(product => (
