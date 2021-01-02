cfd103d27b59fdc57f5b4650e5e9bff4405d68fc
Mario Salinas <mario_1605@live.com.mx>
2021-01-02 11:44:21 -0600

    fix: about page

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,9 +1,7 @@
 import React from 'react'
-import Navbar from '../../components/Navbar/Navbar'
 
 const About = () => {
     return <div>
-        <Navbar />
         Esta es la página de about
     </div>
 }
